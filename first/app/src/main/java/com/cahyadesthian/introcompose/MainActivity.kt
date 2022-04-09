68b340f75c7a1100bab1ac968968622e1a6d8e15
widigdacahya <rizkiwidigda@gmail.com>
2022-04-09 11:24:12 +0700

    modifier
    to specify size our element, that why tadi button kita satu layar

--- first/app/src/main/java/com/cahyadesthian/introcompose/MainActivity.kt
+++ first/app/src/main/java/com/cahyadesthian/introcompose/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
             IntroComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     
