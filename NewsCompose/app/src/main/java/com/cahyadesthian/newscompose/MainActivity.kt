c078cef8227a3431718799d1e4fc777020fd28bd
widigdacahya <rizkiwidigda@gmail.com>
2022-04-09 13:19:39 +0700

    image aspect suqare

--- NewsCompose/app/src/main/java/com/cahyadesthian/newscompose/MainActivity.kt
+++ NewsCompose/app/src/main/java/com/cahyadesthian/newscompose/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -100,7 +101,8 @@ fun NewsListItem(news: News) {
                     .aspectRatio(1f)
                     .weight(1f),
                 painter = rememberAsyncImagePainter(news.imageUrl ),
-                contentDescription = "News Image"
+                contentDescription = "News Image",
+                contentScale = ContentScale.Crop
             )
         }
 
