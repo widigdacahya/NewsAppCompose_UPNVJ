f466712b15be007f444cd37458d23fa747de1bf2
widigdacahya <rizkiwidigda@gmail.com>
2022-04-09 13:05:55 +0700

    head section
    image,box, and coil work here

--- NewsCompose/app/src/main/java/com/cahyadesthian/newscompose/MainActivity.kt
+++ NewsCompose/app/src/main/java/com/cahyadesthian/newscompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.cahyadesthian.newscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.cahyadesthian.newscompose.model.News
+import com.cahyadesthian.newscompose.repository.NewsRepository
 import com.cahyadesthian.newscompose.ui.theme.NewsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +26,17 @@ class MainActivity : ComponentActivity() {
             NewsComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
+                    val newsList = NewsRepository.getAll()
+
+                    Column() {
+                        Header()
+                        NewsList(newList = newsList)
+                    }
+
+
                 }
             }
         }
@@ -30,14 +44,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Header() {
+
+    /*
+    * Box ini kaya container kalo di html
+    * a.k.a wrapper
+    * */
+    Box(
+        modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier.width(160.dp),
+            painter= rememberAsyncImagePainter("https://static.wikia.nocookie.net/logopedia/images/7/75/Google_News_2015.png/revision/latest?cb=20160220081235"),
+            contentDescription= "Logo App",
+        )
+    }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun DefaultPreview() {
-    NewsComposeTheme {
-        Greeting("Android")
-    }
+fun NewsList(newList: List<News>) {
+
 }
