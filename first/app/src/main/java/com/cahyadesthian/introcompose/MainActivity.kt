3d447635625da0eeef5a6c14415a4f375e51422a
widigdacahya <rizkiwidigda@gmail.com>
2022-04-09 11:22:22 +0700

    image

--- first/app/src/main/java/com/cahyadesthian/introcompose/MainActivity.kt
+++ first/app/src/main/java/com/cahyadesthian/introcompose/MainActivity.kt
@@ -3,13 +3,16 @@ package com.cahyadesthian.introcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,22 +32,10 @@ class MainActivity : ComponentActivity() {
                     
                     // Pascal Case untuk nama component -> besar di awal misal FirstName
                     // camelCase untuk attribut
-                    Text(
-                        text = "Hello Cahya\uD83D\uDE01",
-                        color = Color.DarkGray,
-                        fontStyle = FontStyle.Italic,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_favorite),
+                        contentDescription = "Image Favourite",
                     )
-
-                    Text(
-                        text = "Hai Cahya\uD83D\uDE01",
-                        color = Color.DarkGray,
-                        fontStyle = FontStyle.Italic,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Black
-                    )
-                    
                     
                 }
             }
